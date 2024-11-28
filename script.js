9e91d2d91031bd3ad75349b56281125434e05d9f
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2024-11-28 08:58:41 -0300

    up

---
+++ script.js
@@ -0,0 +1,10 @@
+let option = ''
+alert(`Bem vindo, aqui é meu projeto de um rpg funcional!!`)
+let name = prompt(`Digite o nome do seu personagem`)
+alert(`Bem vindo, aqui é meu projeto de um rpg funcional!!`)
+function andar (){
+
+}
+do{
+    option = prompt(``)
+}while()
