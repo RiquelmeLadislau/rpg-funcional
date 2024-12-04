95eb8bc79cc74e1636baace87b54045ee84a6073
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2024-12-04 16:40:18 -0300

    up

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert('oi')
+menu = ''
+let user = prompt(`Nome do seu personagem`)
+
+function character(){
+    const char = {
+        name: prompt(`digite o nome do seu personagem`),
+        gender: prompt(`Qual o gênero dele?`),
+        inventory: prompt(`Escolha`)
+    }
+}
+
+do{
+ menu = prompt(`Bem vindo ${user}, este é um rpg funcional de multiplas escolhas \n 1-Personalize seu personagem\n 2- se aventurar\n 3- sair e excluir tudo`)
+
+switch(menu){
+    case '1':
+        character()
+    case '2':
+    case '3':
+        alert(`Você escoljeu sair`)
+    default:
+        alert(`opção inválida`)
+
+
+
+    }
+}
+while(menu !== '3')
